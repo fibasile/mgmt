db7f6d7a700a116d0591c6c9216dbd7ff8e25792
John Rees <john@bitsushi.com>
2015-01-12 11:30:06 +0000

    migration fix

--- db/migrate/20150112092355_add_invitation_code_to_users.rb
+++ db/migrate/20150112092355_add_invitation_code_to_users.rb
@@ -3,7 +3,7 @@ class AddInvitationCodeToUsers < ActiveRecord::Migration
     add_column :users, :invitation_code, :string, index: true
     User.all.each do |user|
       user.generate_token(:invitation_code)
-      user.save!
+      user.save validate: false
     end
   end
 end
