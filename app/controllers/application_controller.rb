6fc04d0b3d0156f98ee4ed9d7092ed2e1824f10b
John Rees <john@bitsushi.com>
2015-04-07 21:31:57 +0100

    acontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,7 @@ class ApplicationController < ActionController::Base
 private
 
   def current_user
-    if params[:uid] && params[:p] == ENV["P"]
-      @current_user =User.find(params[:uid])
-    elsif session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
