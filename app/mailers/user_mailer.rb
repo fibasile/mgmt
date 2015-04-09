52bbf0860f9af5a02eb147dc5a962c9b58aa912a
John Rees <john@bitsushi.com>
2015-04-09 16:29:01 +0100

    password mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
-
   def password_reset(user)
     @user = user
-    mail to: "#{@user.name} <#{@user.email}>", subject: "Password Reset"
+    mail to: "#{@user.name} <#{@user.email}>", subject: "[IAAC] Password Reset", cc: nil
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -6,7 +6,7 @@ describe UserMailer do
     let(:mail) { UserMailer.password_reset(user) }
 
     it "send user password reset url" do
-      expect(mail.subject).to eq("Password Reset")
+      expect(mail.subject).to eq("[IAAC] Password Reset")
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["notifications@mgmt.iaac.net"])
       expect(mail.body.encoded).to match(edit_password_reset_path(user.password_reset_token))
